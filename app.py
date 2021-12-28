8f9bf231116a7b366d9be8382ef9f02451e2d78e
datascientist1234 <96609488+datascientist1234@users.noreply.github.com>
2021-12-28 19:19:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
-var state = RTCPeerConnection.iceConnectionState
+from streamlit_webrtc import (
+    ClientSettings,
+    VideoTransformerBase,
+    WebRtcMode,
+    webrtc_streamer,
+)
 
-webrtc_streamer(key="example")
+webrtc_ctx = webrtc_streamer(
+        key="object-detection",
+        mode=WebRtcMode.SENDRECV,
+        client_settings=WEBRTC_CLIENT_SETTINGS,
+        video_transformer_factory=MobileNetSSDVideoTransformer,
+        async_transform=True,
+    )
+
